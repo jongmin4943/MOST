046b0b489a7c4c0e5196444baf91d5a0b9011c2a
Jongmin <dbswww@naver.com>
2021-03-07 22:32:08 +0900

    Create Dao, Dto

---
+++ src/comment/CommentDto.java
@@ -0,0 +1,56 @@
+package comment;
+
+public class CommentDto {
+	private String no;
+	private String writer;
+	private String ostNumber;
+	private String content;
+	private String date;
+	
+	public CommentDto() {
+		this("","","","","");
+	}
+	public CommentDto(String no, String writer, String ostNumber, String content, String date) {
+		super();
+		this.no = no;
+		this.writer = writer;
+		this.ostNumber = ostNumber;
+		this.content = content;
+		this.date = date;
+	}
+	public String getNo() {
+		return no;
+	}
+	public void setNo(String no) {
+		this.no = no;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getOstNumber() {
+		return ostNumber;
+	}
+	public void setOstNumber(String ostNumber) {
+		this.ostNumber = ostNumber;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	@Override
+	public String toString() {
+		return "CommentDto [no=" + no + ", writer=" + writer + ", ostNumber=" + ostNumber + ", content=" + content
+				+ ", date=" + date + "]";
+	}
+}
---
+++ src/ost/OstDto.java
@@ -0,0 +1,43 @@
+package ost;
+
+public class OstDto {
+	private String no;
+	private String ostName;
+	private String artist;
+	private String likes;
+	public OstDto() {
+		this("","","","");
+	}
+	public OstDto(String no, String ostName, String artist, String likes) {
+		super();
+		this.no = no;
+		this.ostName = ostName;
+		this.artist = artist;
+		this.likes = likes;
+	}
+	public String getNo() {
+		return no;
+	}
+	public void setNo(String no) {
+		this.no = no;
+	}
+	public String getOstName() {
+		return ostName;
+	}
+	public void setOstName(String ostName) {
+		this.ostName = ostName;
+	}
+	public String getArtist() {
+		return artist;
+	}
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	public String getLikes() {
+		return likes;
+	}
+	public void setLikes(String likes) {
+		this.likes = likes;
+	}
+	
+}
